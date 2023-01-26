9f2af938ee6457a7295553107214d198aac34bc8
unknown <vanessasinoki@yahoo.com.br>
2023-01-26 14:34:08 -0300

    Home-NavBar
    . Logo
    . Mudança de cor ao clicar no menu
    . Menus linkados
    . Alinhamento

---
+++ src/components/NavBar.vue
@@ -0,0 +1,78 @@
+<template>
+  <header>
+    <div class="nav">
+      <img src="@/assets/LogoRodrigoAdminhorizontal.png" class="img-logo" />
+
+      <div class="menu" id="nav">
+        <router-link to="/">(41)82418142</router-link> |
+        <router-link to="/about">Sobre</router-link> |
+        <router-link to="/services">Serviços</router-link> |
+        <router-link to="/demonstration">Demonstração</router-link> |
+        <router-link to="/contact">Contato</router-link> |
+        <router-link to="/login">Login</router-link> |
+        <router-link to="/boletos">2ª via Boletos</router-link> |
+      </div>
+      <router-view />
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NavBar',
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+@import url("https://use.fontawesome.com/releases/v5.9.0/css/all.css");
+* {
+  text-decoration: none;
+  list-style: none;
+}
+header {
+  font-family: roboto, sans-serif;
+  background-position: center;
+  background-size: cover;
+  height: 100vh;
+}
+.nav {
+  height: 80px;
+  width: 100%;
+  background: #d2dae0;
+}
+.img-logo {
+  width: 220px;
+  height: 135px;
+  padding-top: 20px;
+  margin-top: -55px;
+  margin-left: 20px;
+  border: 2px solid black;
+}
+
+.menu {
+  float: right;
+  margin-right: 40px;
+  margin-top: 5px;
+}
+.menu a {
+  text-decoration: none;
+  color: black;
+  margin: 0 10px;
+  line-height: 70px;
+}
+$color: #fff;
+
+a:focus {
+  color: #42b983;
+  font-weight: bold;
+  transition: 0.5s;
+}
+
+@media only screen and (max-width: 690px) {
+  label {
+    display: block;
+    cursor: pointer;
+  }
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 import Home from '../views/Home.vue';
+import About from '../views/About.vue';
+import Services from '../views/Services.vue';
+import Demonstration from '../views/Demonstration.vue';
+import Contact from '../views/Contact.vue';
+import Login from '../views/Login.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -10,10 +15,28 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: About,
+  },
+  {
+    path: '/services',
+    name: 'Services',
+    component: Services,
+  },
+  {
+    path: '/demonstration',
+    name: 'Demonstration',
+    component: Demonstration,
+  },
+
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
   },
 ];
 
